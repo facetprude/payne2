e17ea819a67a6774f35449c3c8a1851bb396f374
facetprude <66576277+facetprude@users.noreply.github.com>
2021-01-12 19:24:14 -0800

    Update download.py

--- download.py
+++ download.py
@@ -5,6 +5,9 @@ import pandas as pd
 import requests
 
 url = "https://theodorepayne.org/plants-and-seeds/nursery/inventory/"
-headers = {"User-agent": os.environ["CONTACT_INFO"]}
+CONTACT_INFO = os.environ["CONTACT_INFO"]
+if not CONTACT_INFO:
+  raise ValueError("No contact information provided.")
+headers = {"User-agent": CONTACT_INFO}
 html = requests.get(url, headers=headers).text
 pd.read_html(html, header=0)[0].to_csv("data.csv", index=False)
